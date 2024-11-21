3a1028d2a4d528092b37bcdbc8fb48e61f237c98
Shoaib Shaikh <shaikhshoaib2k21@gmail.com>
2024-11-21 18:34:44 +0530

    hello world

---
+++ hello_world.py
@@ -0,0 +1,21 @@
+#Function for raw user input
+a=input('Enter your name')
+
+
+#Print format #1, with spaces
+print('Hello World',a)
+
+
+#Print format #2, without spaces
+print('Hello World'+a)
+
+
+#Print format #3, with format specifiers
+print('Hello World %s'%a)
+
+
+# Output:
+# Enter your nameShoaib
+# Hello World Shoaib
+# Hello WorldShoaib
+# Hello World Shoaib
