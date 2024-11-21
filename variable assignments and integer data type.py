8490db6080f8012c15ae0c7515009b45ce8285c3
Shoaib Shaikh <shaikhshoaib2k21@gmail.com>
2024-11-21 18:41:06 +0530

    variable assignments and integer data type

---
+++ variable assignments and integer data type.py
@@ -0,0 +1,17 @@
+a=111
+b=20
+
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+print("Modulus:",a%b)
+print("Integral division:",a//b)
+
+# Output:
+# Addition: 131
+# Subtraction: 91
+# Multiplication: 2220
+# Division: 5.55
+# Modulus: 11
+# Integral division: 5
