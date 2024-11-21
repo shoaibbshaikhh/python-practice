323bdff13c1477176b31ea5964fd2f20109f37dd
Shoaib Shaikh <shaikhshoaib2k21@gmail.com>
2024-11-21 18:49:42 +0530

    string indexing

---
+++ 05_string_indexing.py
@@ -0,0 +1,12 @@
+a='hello world'
+
+print("Element at 1st position:",a[0])
+print("Element at last position:",a[-1])
+print("Element at 2nd last position:",a[-2])
+print("Element at 4th position:",a[3])
+
+# Output:
+# Element at 1st position: h
+# Element at last position: d
+# Element at 2nd last position: l
+# Element at 4th position: l
