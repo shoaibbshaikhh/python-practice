1b47a56371914689a15387b0be8b6374f0f63f29
Shoaib Shaikh <shaikhshoaib2k21@gmail.com>
2024-11-21 18:42:54 +0530

    variable assignments and integer data type

---
+++ floating point numbers.py
@@ -0,0 +1,18 @@
+#a stores result of 5/2
+a=5/2
+
+#b stores result of 7/3
+b=7/3
+
+#Gives 2.5
+print("a=",a)
+
+#Update a
+a=100
+
+#c stores result of a/b
+c=a/b
+
+print("a=",a)
+print("b=",b)
+print("Result c=",c)
