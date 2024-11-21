3ff254675bf048d05e0e1f524dcdebe5d43814cd
Shoaib Shaikh <shaikhshoaib2k21@gmail.com>
2024-11-21 18:47:47 +0530

    string

---
+++ 04_intro_to_strings.py
@@ -0,0 +1,11 @@
+a='hello world I\'m root'
+b='subscribe'
+
+print("Normal print:",a,b)
+print("Concatenation:",a+b)
+print("Format specifier: %s %s"%(a,b))
+
+# Output:
+# Normal print: hello world I'm root subscribe
+# Concatenation: hello world I'm rootsubscribe
+# Format specifier: hello world I'm root subscribe
