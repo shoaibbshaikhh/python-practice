be2f2abb570fe774cf581ba0f78be22d4c13e9dc
Shoaib Shaikh <shaikhshoaib2k21@gmail.com>
2024-11-26 18:24:17 +0530

    Print Formatting and Precision

--- 08_print_formatting_and_precision.py
+++ 08_print_formatting_and_precision.py
@@ -7,10 +7,19 @@ a=1000.4233746327847836723
 #r will be printed after s because of indexing
 print('follow {1} {0}'.format(r,s))
 
+# Output:
+# follow shoaib shaikh
+
 
 #With f specifier (Python >=3.6)
 print(f"follow {s} {r}")
 
+# Output:
+# follow shoaib shaikh
+
 
 #Precision and Width of floating point numbers
 print(f"{a:1.3f}")
+
+# Output:
+# 1000.423
